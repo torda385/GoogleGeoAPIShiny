d2f3cb7b3258336bd31563b14d0c12abcf0448a1
torda385 <torda385@student.liu.se>
2016-09-28 11:40:41 +0200

    creation of shiny package
    working on the server file

--- R/ggas.R
+++ R/ggas.R
@@ -21,23 +21,68 @@ if(!require(leaflet)) install_github("rstudio/leaflet")
 #install shiny
 if(!require(shiny)) install_github("shiny")
 #install GoogleGeoAPI app
-#**************#
+if(!require(GoogleGeoAPI)) install_github("Rchieve/GoogleGeoAPI")
 
+shinyApp(
+  ui <- fluidPage(
+    titlePanel(
+      "Geocoding with GoogleGeoAPI"
+    ),
+    
+    sidebarPanel(
+      fluidRow(
+        radioButtons("first_choice",
+                  h1("Input Type"),
+                  c("Address","Coordinates")),
+        h1("Input Location"),
+        bsCollapse(
+          bsCollapsePanel(tile="details",
+                          uiOutput("second_choice")),
+          id="collapser",multiple=FALSE, open=NULL
+        )
+      ),
+      hr(),
+      fluidRow(
+        sliderInput("slider1",label=h3("Slider"),min=0,max=100,value=10
+        )
+      ),
+      fluidRow(
+        verbatimTextOutput("value")
+      ),
+      hr()
+  ),
+    mainPanel(
+      leafletOutput('myMap'),
+      p()
+    )
+  ),
 
-
-ggas <- function (lng, lat, z){
-  if(missing(z)) {
-    z=10
-  }
-  #create the base map
-  (m<-leaflet() %>% addTiles())
-  img<-readPNG("~/repos/Creating-maps-in-R/figure//shiny_world.png")
-  grid.raster(img)
-  #add a marker to the map
-  (m2 <- m %>% 
-    setView(lng=lng,lat=lat,zoom=z) %>% #add the location and set map size from data
-    addMarkers(lng,lat)) #add the marker to the coordinates
   
+  server <- function(input,output,session){
+      #change ui component depending on input$input_type
+      switch(input$first_choice,
+             "Address"=textInput("addr1","Address", value="Linköping University, 58183, Linköping"),
+             "Coordinates"=(textInput("latitude","Latitude",value="58.400101"),textInput("longitude","Longitude",value="15.576227"))
+      )
+      
+      
+      
+    #set zoom
+    output$value <- renderPrint({input$slider1})
+    if (input$type1 == 1){
+      address <- input$addr1
+    } else {
+      latitude
+    }
 
-
-}
+    textPop<-
+    longitude<-
+    latitude<-
+  #create the base map
+  map=leaflet() %>%
+    addTiles() %>%
+    setView(lng=longitude,lat=latitude,zoom=output$value) %>% #add the location and set map size from data
+    addPopups(lng=longitude,lat=latitude,popup=textPop) #add the marker to the coordinates
+    output$myMap=renderLeaflet(map) #create map from output
+  }
+)
